5b618626df80397df7e13bcd8764e5efc563e4ae
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2015-03-30 18:03:00 -0400

    Adding leaflet map

---
+++ velLeafletMap.R
@@ -0,0 +1,26 @@
+library(leaflet) #See http://rstudio.github.io/leaflet/
+library(RColorBrewer)
+library(classInt)
+source("./velLoad.R")
+
+#colors
+palB <- brewer.pal(11, "Spectral")
+pal <- colorRampPalette(palB)
+ncol <- 100
+vel$colors <- findColours(classIntervals(vel$effect, ncol, style="quantile"), pal(ncol))
+
+#make a base leaflet map
+#mapquest satellite imagery
+#ESRI - http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}
+#OpenMaps - http://otile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg
+velLeaf <- leaflet() %>% addTiles(urlTemplate="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}")
+
+#Make some info for popups
+vel$popup <- with(vel, paste0("Log(SR2/SR1)/Duration: ", round(effect,3), "<br>Duration: ", Duration, 
+                             " years<br>Start: ", Year1_min, "<br>End: ", 
+                             Year2_max, "<br>Driver: ", Driver))
+
+#Make a leaflet map
+velLeaf %>% addCircleMarkers(data=vel,
+                       lat = ~ Latitude, lng = ~ Longitude, popup = ~popup,
+                        radius=~Duration/7, color=~colors, fill=~colors)
